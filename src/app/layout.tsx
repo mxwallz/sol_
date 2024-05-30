d9ac1a77278751127a2762a15637bcda0767aac2
Marcus Walls <marcuswalls.142@gmail.com>
2024-05-30 15:04:08 -0500

    removed defaiult theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             <ThemeProvider
               attribute="class"
               enableSystem={false}
-              defaultTheme="dark"
+            
             >
               <Header />
               {children}
